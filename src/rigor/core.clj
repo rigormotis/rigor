cbbc75327a384a06390ceb29d82b2bc818326620
Josh Adams <jadams1217@gmail.com>
2012-07-17 00:03:04 -0400

    Added some initial display surface functions
    Using LWJGL to create a rendering surface.  Testing out the use of
    protocols with multimethods and recrods for a level of genericity.

--- src/rigor/core.clj
+++ src/rigor/core.clj
@@ -1,7 +1,13 @@
 (ns rigor.core
+  (:use [rigor.surface])
   (:gen-class))
 
 (defn -main
   "I don't do a whole lot."
   [& args]
+  (let [settings {:surface-system :LWJGL}
+        surf (create-surface settings)]
+    (create surf 600 400)
+    (Thread/sleep 5000)
+    (destroy surf))
   (println "Hello, World!"))
---
+++ src/rigor/surface.clj
@@ -0,0 +1,31 @@
+(ns rigor.surface
+  (:import [org.lwjgl.opengl Display DisplayMode])
+  (:gen-class))
+
+(defprotocol Surface
+  (create [this width height])
+  (destroy [this])
+  (resize [this width height])
+  (set-fullscreen [this fs])
+  (fullscreen? [this]))
+
+(defmulti create-surface :surface-system)
+
+(defrecord LWJGLSurface []
+  Surface
+  (create [this width height]
+    (Display/setDisplayMode (DisplayMode. width height))
+    (Display/create))
+  (destroy [this]
+    (Display/destroy))
+  (resize [this width height]
+    (println "TODO: implement Surface/display"))
+  (set-fullscreen [this fs]
+    (Display/setFullscreen fs))
+  (fullscreen? [this]
+    (Display/isFullscreen)))
+
+(defmethod create-surface :LWJGL [settings]
+  (LWJGLSurface.))
+
+
